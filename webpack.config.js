c7c96c950f0114708f67110646efe671a89a3333
Carl Chen <carlchen214@gmail.com>
2016-07-14 22:07:21 -0700

    chores/ update eslint, package.json & webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,17 @@
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   entry: './client/src/index.jsx',
-  output: {
-    path: './client/src',
-    filename: 'bundle.js',
-=======
-  entry: './client/app.js',
-=======
-  entry: './client/src/index.jsx',
->>>>>>> chores/ update webpack to use jsx & correct filepath for index.jsx
   output: {
     path: './client/src',
     filename: 'bundle.js'
->>>>>>> (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies
   },
   devServer: {
     inline: true,
     contentBase: './client',
-<<<<<<< HEAD
-    port: 8100,
-=======
     port: 8100
->>>>>>> (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies
   },
   module: {
     loaders: [
       {
-<<<<<<< HEAD
-<<<<<<< HEAD
         test: /\.jsx$/,
         exclude: /node_modules/,
         loader: 'babel',
@@ -36,15 +19,3 @@ module.exports = {
     ],
   },
 };
-=======
-        test: /\.js$/,
-=======
-        test: /\.jsx$/,
->>>>>>> chores/ update webpack to use jsx & correct filepath for index.jsx
-        exclude: /node_modules/,
-        loader: 'babel'
-      }
-    ]
-  }
-}
->>>>>>> (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies
