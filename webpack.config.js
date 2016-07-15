5f4517341bf75c6b368224144b041933721179c0
Carl Chen <carlchen214@gmail.com>
2016-07-14 21:19:06 -0700

    (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
 module.exports = {
+<<<<<<< HEAD
   entry: './client/src/index.jsx',
   output: {
     path: './client/src',
     filename: 'bundle.js',
+=======
+  entry: './client/app.js',
+  output: {
+    path: './client',
+    filename: 'bundle.js'
+>>>>>>> (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies
   },
   devServer: {
     inline: true,
     contentBase: './client',
+<<<<<<< HEAD
     port: 8100,
+=======
+    port: 8100
+>>>>>>> (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies
   },
   module: {
     loaders: [
       {
+<<<<<<< HEAD
         test: /\.jsx$/,
         exclude: /node_modules/,
         loader: 'babel',
@@ -19,3 +31,12 @@ module.exports = {
     ],
   },
 };
+=======
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
+>>>>>>> (setup) Config webpack for client/app.js, set up webpack & webpack dev server as devDependencies
