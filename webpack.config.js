2dec3b2410b0348ca21eca6f9482ac0b77a1a8fe
Carl Chen <carlchen214@gmail.com>
2016-07-14 22:07:21 -0700

    chores/ update eslint, package.json & webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ module.exports = {
   entry: './client/src/index.jsx',
   output: {
     path: './client/src',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     inline: true,
     contentBase: './client',
-    port: 8100
+    port: 8100,
   },
   module: {
     loaders: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        loader: 'babel'
-      }
-    ]
-  }
-}
+        loader: 'babel',
+      },
+    ],
+  },
+};
