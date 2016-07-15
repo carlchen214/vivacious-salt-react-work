8ff811f8aa5da2d9802e714035f7f3dac43876e3
Carl Chen <carlchen214@gmail.com>
2016-07-14 23:04:26 -0700

    Make express server serve up static files (index.html)

--- server/server.js
+++ server/server.js
@@ -7,10 +7,12 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '../client/public'));
 
-app.get('/', function(req, res) {
-  res.status(200).json({ message: 'Hello Vivacious Salt!' });
-})
+// app.get('/', function(req, res) {
+//   res.status(200).json({ message: 'Hello Vivacious Salt!' });
+//   res.send
+// })
 
 app.listen(port, function() {
   console.log('Listening on port', port);
